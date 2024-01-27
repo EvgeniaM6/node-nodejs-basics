b0529e558e522e28a691ef7512fc0606c37e9b2e
EvgeniaM6 <evgminukova6@gmail.com>
2024-01-27 15:34:02 +0200

    feat: complete streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { stdout } from 'node:process';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const fileReadStream = createReadStream(filePath);
+    fileReadStream.pipe(stdout);
 };
 
 await read();
