0cc9bab2ce613464780d3750d20fccdd3b8dfcd5
EvgeniaM6 <evgminukova6@gmail.com>
2024-01-27 14:31:17 +0200

    feat: complete command line interface task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const resultArr = Object.entries(env).filter(([envKey, _]) => {
+        return envKey.slice(0, 4) === 'RSS_';
+    }).map(([envKey, envVal]) => {
+        return `${envKey}=${envVal}`;
+    });
+    console.log(resultArr.join('; '));
 };
 
 parseEnv();
